4e1de732ef45a4db708024686814c0dc742dc1f5
Shompoth KHAN <dadoulan@gmail.com>
2021-02-18 09:22:03 +0100

    HTML&CSS : creation design article, JS :  envoie formulaire et recuperation article (accueil et form)

--- src/form/form.js
+++ src/form/form.js
@@ -1,4 +1,56 @@
 import "../assets/styles/styles.scss";
 import "./form.scss";
 
-console.log("form");
+const form = document.querySelector("form");
+const errorElement = document.querySelector("#errors");
+let errors = [];
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  // Creation cle / valeur (iterable)
+  const formData = new FormData(form);
+  const article = Object.fromEntries(formData.entries());
+  if (formIsValid(article)) {
+    try {
+      const json = JSON.stringify(article);
+      const response = await fetch("https://restapi.fr/api/article", {
+        method: "POST",
+        body: json,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const body = await response.json();
+      console.log(body);
+    } catch (e) {
+      console.error("e : ", e);
+    }
+  }
+});
+
+const formIsValid = (article) => {
+  if (
+    !article.author ||
+    !article.img ||
+    !article.category ||
+    !article.title ||
+    !article.content
+  ) {
+    errors.push("Vous devez remplir tous les champs.");
+  } else {
+    errors = [];
+  }
+  if (errors.length) {
+    let errorHTML = "";
+    errors.forEach((e) => {
+      errorHTML += `<li>${e}</li>`;
+    });
+    errorElement.innerHTML = errorHTML;
+    errors = [];
+    return false;
+  } else {
+    errorElement.innerHTML = "";
+    return true;
+  }
+};
